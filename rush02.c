ccb4c41588937e6705cad7db043ff88503bfe433
choisk0531 <78864978+choisk0531@users.noreply.github.com>
2021-02-20 12:22:30 +0900

    Rush02
    plz check

---
+++ rush02.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void rush01(int a, int b) {
+	printf("\n");
+	int i = 1;
+	int j = 1;
+	while (j <= b) {
+		while (i <= a) {
+			if (j == 1) {
+				if (i == 1 || i == a)
+					printf("A");
+				else
+					printf("B");
+			}
+			else if (j == b) {
+				if (i == 1 || i == a)
+					printf("C");
+				else
+					printf("B");
+			}
+			else {
+				if (i == 1 || i == a)
+					printf("B");
+				else
+					printf(" ");
+			}
+			i++;
+		}
+		i = 1;
+		printf("\n");
+		j++;
+	}
+}
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	rush01(a, b);
+	return 0;
+}
