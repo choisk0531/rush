16261ac1289706457add9421cd18d1b8b181e53b
choisk0531 <78864978+choisk0531@users.noreply.github.com>
2021-02-23 13:14:06 +0900

    detail
    detail

--- rush04.c
+++ rush04.c
@@ -3,13 +3,13 @@ void rush04(int a, int b) {
 	printf("\n");
 	int i = 1;
 	int j = 1;
-	while(j<=b){
-		while(i<=a) {
-			if ((i == 1 && j == 1)|| (i==1 && j==b))
+	while (j <= b){
+		while (i <= a) {
+			if ((i == 1 && j == 1)|| (i == a && j == b))
 				printf("A");
-			else if ((i == a && j == 1) || (i == a && j == b))
+			else if ((i == a && j == 1) || (i == 1 && j == b))
 				printf("C");
-			else if(i==1 || i==a || j==1 || j==b)
+			else if(i == 1 || i == a || j ==1  || j == b)
 				printf("B");
 			else
 				printf(" ");
